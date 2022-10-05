f3c8fc00c0e6acebcbc2b2643fb9cf1ca61e6077
Lester Ivan <lesterivan.dalaguit@gmail.com>
2022-10-05 21:16:15 +0800

    Ready for JQuery

--- utils/conn.php
+++ utils/conn.php
@@ -4,5 +4,10 @@ $username = "root";
 $password = "";
 $database = "insti-db";
 
+// CONNECTED NAKA SA DATABASE
 $connect = mysqli_connect($server, $username, $password, $database) or die("Error: ". mysqli_connect_error($connect));
+
+if($connect) {
+    echo("<script>console.log('Connected!')</script>");
+}
 ?>
